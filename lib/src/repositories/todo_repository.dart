be7dfd752ef0cf28f9c82a66b81673b154a545cb
Matheus Carvalho do Carmo <matheuscarvalho@windowslive.com>
2021-07-01 23:22:13 -0300

    testa o todo repository

--- lib/src/repositories/todo_repository.dart
+++ lib/src/repositories/todo_repository.dart
@@ -2,14 +2,17 @@ import 'package:dio/dio.dart';
 import 'package:todo_list/src/models/todo_model.dart';
 
 class TodoRepository {
-  final dio = Dio();
-  final baseUrl = 'https://jsonplaceholder.typicode.com/todos/1';
+  late Dio dio;
+  final baseUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  TodoRepository([Dio? client]) : this.dio = client ?? Dio();
 
   Future<List<TodoModel>> fetchTodos() async {
     final response = await dio.get(baseUrl);
     final list = response.data as List;
-    List<TodoModel> todos =
-        list.map((e) => TodoModel.fromJson(e)) as List<TodoModel>;
+
+    List<TodoModel> todos = list.map((e) => TodoModel.fromJson(e)).toList();
+
     return todos;
   }
 }
